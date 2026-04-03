9d94a5819a599bd88b7e64cb83cf1585aaf748cc
Bart Kleijngeld <bartkl@gmail.com>
2026-04-03 13:21:46 +0200

    Better logging

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Cleaning up output directory…"
+echo 'Cleaning up output directory `docs/`…'
 rm -rf docs
 
-echo "Generating HTML for presentations…"
+echo 'Generating HTML for presentations…'
 cp -r src docs
 find docs -type f -name index.adoc -exec npx asciidoctor-revealjs -r asciidoctor-kroki {} \+
-echo -e "done.\n"
+echo -e 'done.\n'
